c74791e3d7b9ca8bfeec0dfb6c570113112a388f
Alex Croden <alexcroden@hotmail.com>
2023-06-29 16:58:28 +0100

    set up API

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,15 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABSE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABSE_URL;
+  config.max = 2
+}
+
+module.exports = new Pool(config);
