02734c148c7b04aa9b1adf244c03bc6d17482a75
Alex Croden <alexcroden@hotmail.com>
2023-06-30 09:22:32 +0100

    fixed .env. typo

--- db/connection.js
+++ db/connection.js
@@ -4,14 +4,14 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && !process.env.DATABSE_URL) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
 const config = {};
 
 if (ENV === 'production') {
-  config.connectionString = process.env.DATABSE_URL;
+  config.connectionString = process.env.DATABASE_URL;
   config.max = 2
 }
 
